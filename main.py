703ac80d64ffac75c8436a0ceff0ddd6331b335b
Alperen <enisalprn@gmail.com>
2026-02-04 21:23:40 +0300

    feat: implement plate alignment and OCR error handling utilities for Turkish plates

--- main.py
+++ main.py
@@ -25,3 +25,42 @@ def init_db():
     connection.commit()
     connection.close()
 
+def order_points(pts):
+    rect = np.zeros((4, 2), dtype="float32")
+
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    
+    return rect
+
+def fix_plate_text_errors(text):    
+    dict_num_to_char = {'0': 'O', '1': 'I', '2': 'Z', '5': 'S', '8': 'B'}
+    dict_char_to_num = {'O': '0', 'I': '1', 'Z': '2', 'S': '5', 'B': '8'}
+
+    text = text.replace(" ", "")
+    corrected= list(text)
+    length = len(text)
+    if length<5:
+        return
+    for i in range(length):
+        if i<2:
+            if corrected[i] in dict_char_to_num:
+                corrected[i]=dict_char_to_num[corrected[i]]
+
+        elif i==2:
+            if corrected[i] in dict_num_to_char:
+                corrected[i]=dict_num_to_char[corrected[i]]
+                
+        elif i>=length-2:
+            if corrected[i] in dict_char_to_num:
+                corrected[i]=dict_char_to_num[corrected[i]]
+        elif i == 3  and length == 7:
+                if corrected[i] in dict_num_to_char:
+                    corrected[i] = dict_num_to_char[corrected[i]]
+    return "".join(corrected)
+
